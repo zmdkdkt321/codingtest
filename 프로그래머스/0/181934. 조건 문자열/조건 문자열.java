fb35dcb3f11c0be0f410416b6b3ba2db147aa9e7
SangYeon Kim <zmdkdkt321@gmail.com>
2024-03-06 14:34:37 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        int answer = 0;
-        if(ineq.equals("<")){
-            if(n < m){
-                return 1;
-            }
-        }else{
-            if(n > m){
-                return 1;
-            }
-        }
-        if(eq.equals("=")){
-            return n==m?1:0;
-        }
-        return 0;
+        return ineq.equals("<")?(n<m)?1:eq.equals("=")?(n==m)?1:0:0:(n>m)?1:eq.equals("=")?(n==m)?1:0:0;
     }
 }
