981228621f18932e9ddb17d61fc47ef6efd84b4e
SangYeon Kim <zmdkdkt321@gmail.com>
2024-03-13 14:09:17 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.06 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -3,47 +3,31 @@ class Solution {
     public int solution(int a, int b, int c, int d) {
         int[] dice = {a,b,c,d};
         int equalCount = 0; // 6==a,a,a,a 2==a,a,b,b 1 == a,a,b,c 0 == a,b,c,d
-        int pairNum = 0;
+        int pairNum = -1;
+        int anotherNum = -1;
         for(int i = 0; i < 4; i++){
             for(int j = i+1; j < 4; j++){
                 if(dice[i] == dice[j]){
                     equalCount++;
-                    pairNum = dice[i];
+                    if(pairNum == -1)pairNum = dice[i];
                 }
             }
+            if(pairNum != dice[i]) anotherNum = dice[i];
         }
         
         switch(equalCount){
             case 6:
                 return 1111*pairNum;
             case 3:
-                int anotherNum = 0;
-                for(int die:dice){
-                    if(pairNum != die){
-                        anotherNum = die;
-                        break;
-                    };
-                }
                 return (int)Math.pow(pairNum*10 + anotherNum,2);
             case 2:
-                int anotherPairNum = 0;
-                for(int die:dice){
-                    if(pairNum != die){
-                        anotherPairNum = die;
-                        break;
-                    };
-                }
-                return (anotherPairNum+pairNum)*Math.abs(anotherPairNum-pairNum);
+                return (anotherNum+pairNum)*Math.abs(anotherNum-pairNum);
             case 1:
                 int q = 0;
-                int r = 0;
                 for(int die:dice){
-                    if(pairNum != die){
-                        if(q == 0) q = die;
-                        else r = die;
-                    }
+                    if((pairNum != die) && (anotherNum !=die)) q = die;
                 }
-                return q*r;
+                return anotherNum*q;
             case 0:
                 return Arrays.stream(dice).min().getAsInt();
         }
