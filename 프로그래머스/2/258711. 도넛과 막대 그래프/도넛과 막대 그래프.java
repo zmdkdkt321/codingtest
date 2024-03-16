dca91ff92cecf0dcf49b7524eb3ca113c65310dc
SangYeon Kim <zmdkdkt321@gmail.com>
2024-03-16 12:25:44 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 12.76 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,62 +1,27 @@
-import java.util.*;
-
 class Solution {
+    static int N = 1_000_000;
+
     public int[] solution(int[][] edges) {
-        int[] answer = {};
-        Map<Integer,Integer> nodeCount = new HashMap<>();
-        //들어오는게 없으며 2개 이상 가르키는 노드 = 생성 노드
-        //생성 노드에서 뻗는 간선의 수 = 그래프의 수
-        //받기만 하는 노드의 숫자 == 막대 그래프의 수
-        //생성 노드를 제외하고 2개 보내는 친구는 8자 그래프
-        //그래프의 숫자 - 막대 그래프의 수 - 8자 그래프의 수 = 도넛 그래프의 수
-        
-        //노드의 수를 구하기
-        for(int[] edge:edges){
-            if(nodeCount.get(edge[0]) == null){
-                nodeCount.put(edge[0],0);
+        int[] ingoing = new int[N];
+        int[] outgoing = new int[N];
+        for(int[] edge : edges) {
+            outgoing[edge[0]-1]++;
+            ingoing[edge[1]-1]++;
+        }
+        int created = 0;
+        int eight = 0;
+        int stick = 0;
+        for(int i=0; i<N; i++) {
+            if(outgoing[i] >= 2) {
+                if(ingoing[i] == 0) {
+                    created = i;
+                }else {
+                    eight++;
+                }
+            }else if(outgoing[i] == 0 && ingoing[i] > 0){
+                stick++;
             }
-            if(nodeCount.get(edge[1]) == null){
-                nodeCount.put(edge[1],1);
-            }else{
-                nodeCount.put(edge[1],nodeCount.get(edge[1])+1);
-            }
         }
-        //노드의 수
-        int nodeSize = nodeCount.size();
-        //막대 그래프의 수는 쉽게 구함 = 주는게 없는 노드의 수
-        //막대 그래프의 노드의 수도 쉽게 구함
-        //도넛 그래프의 노드 수 + 8그래프의 노드의 수= z
-        //nodeSize - 주는게 없는 노드의 수 -1
-        //노드의 수 = 
-        //간선의 수 = x(z)+y()+(x+y)
-        //x = 도넛 그래프 수, y = 8그래프 수
-        
-        //생성 노드 무저저저적권 필요하다
-        
-        //
-        int graghCount = 0;
-        //해당 인덱스에서 출발하는 간선의 수
-        int startEdge[] = new int[nodeSize+5];
-        //해당 인덱스에 도착하는 간선의 수
-        int endEdge[] = new int[nodeSize+5];
-        
-        for(int[] edge:edges){
-            startEdge[edge[0]]++;
-            endEdge[edge[1]]++;
-        }
-        
-        int createdNode = -1;
-        int stickGraphCount = 0;
-        int eigthGraphCount = 0;
-        for(int i = 1; i <= nodeSize; i++){
-            if(endEdge[i] == 0){
-                if(startEdge[i] >= 2) createdNode = i;
-            }else if(startEdge[i] == 2) eigthGraphCount++;
-            else if(startEdge[i] == 0) stickGraphCount++;
-        }
-        int donutGraphCount = startEdge[createdNode]-stickGraphCount-eigthGraphCount;
-        
-        int result[] = {createdNode,donutGraphCount,stickGraphCount,eigthGraphCount};
-        return result;
+        return new int[] {created+1,outgoing[created]-eight-stick,stick,eight};
     }
 }
