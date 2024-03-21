bee23cf02b3bfff80e6a3de05b85646500aa2ecb
SangYeon Kim <zmdkdkt321@gmail.com>
2024-03-21 16:20:44 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length-2; i++){
+            sum = 0;
+            sum += nums[i];
+            for(int j=i+1; j < nums.length-1; j++){
+                sum += nums[j];
+                for(int k=j+1; k < nums.length; k++){
+                    sum+=nums[k];
+                    list.add(sum);
+                    sum-=nums[k];
+                }
+                sum -= nums[j];
+            }
+        }
+        boolean flag;
+        for(int setNum:list){
+            flag = true;
+            for(int i = 2; i <= setNum/2; i++){
+                if(setNum%i == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) answer++;
+        }
+        return answer;
+    }
+}
