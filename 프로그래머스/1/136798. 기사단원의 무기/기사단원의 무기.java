6400cf765a05ef0af37f9c4b8e944e9fe93b8636
SangYeon Kim <zmdkdkt321@gmail.com>
2024-03-21 17:26:36 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.16 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        int[] nArr = new int[number+1];
+        int[] nArr = new int[number];
         for(int i = 1; i <= number; i++){
             for(int j = i; j <= number;j+=i){
-                nArr[j]++;
+                nArr[j-1]++;
             }
         }
         
