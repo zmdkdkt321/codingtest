9f8692379c878dcb7ac29947e09fcbf74c7dc7ae
SangYeon Kim <zmdkdkt321@gmail.com>
2024-04-19 09:54:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 84.92 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class Solution {
+        public int solution(int[] elements) {
+            Set<Integer> set = new HashSet<>();
+            int[] dp = new int[elements.length];
+            for(int len = 1;len <= elements.length; len++){
+                for(int i = 0;i<elements.length;i++){
+                    dp[i] += elements[(len+i-1)%elements.length];
+                    set.add(dp[i]);
+                }
+            }
+            return set.size();
+        }
+    }
